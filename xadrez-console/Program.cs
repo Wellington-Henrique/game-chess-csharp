8f9dfa4658c841258d6e85b97084c29e13a4d0b9
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-25 21:43:06 -0300

    Adding Screen method

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,7 +5,7 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
             Board board = new Board(8, 8);
-            Console.WriteLine(board);
+            Screen.PrintBoard(board);
             Console.ReadLine();
         }
     }
